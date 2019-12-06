877862a8b4589edbe2b7317e2b71fb8812d153ee
ksrrock <kshivamranjan.cse18@itbhu.ac.in>
2019-12-06 16:35:20 +0530

    Training and Placement Portal

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kumar/Desktop/CAMPUS_PLACEMENT_MANAGEMENT_SYSTEM/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
